28b16a43d7ad7dc6eabb1875bd3cf7420cc879dc
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2015-09-11 23:31:57 +0200

    in my case strcmp_P doesn't work -> helper function

--- AHelper.h
+++ AHelper.h
@@ -35,4 +35,21 @@ public:
 	{
 		return (abs(number)<1?0:log10(abs(number)))+2+prec+(number<0?1:0);
 	}
+	///Compares PROGMEM strings. I didn't manged to use strcmp_P function
+	static bool compare_F(const __FlashStringHelper *str1,const __FlashStringHelper *str2)
+	{
+		size_t len1 = strlen_P((const char PROGMEM *)str1);
+		size_t len2 = strlen_P((const char PROGMEM *)str2);
+		if(len1!=len2)
+			return false;
+		unsigned char c1,c2;
+		for (int i=0; i<len1; i++)
+		{
+			c1 = pgm_read_byte(&((const char PROGMEM *)str1)[i]);
+			c2 = pgm_read_byte(&((const char PROGMEM *)str2)[i]);
+			if(c1!=c2)
+				return false;
+		}
+		return true;
+	}
 };
