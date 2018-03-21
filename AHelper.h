cfede2e78769b5113d351bc86eb34f5dc032301f
AndreiDegtiarev <Andrey.Degtyarev@gmail.com>
2018-03-21 22:14:06 +0100

    Update license info

--- AHelper.h
+++ AHelper.h
@@ -1,7 +1,7 @@
 #pragma once
 /*
   AFrame - Arduino framework library for ASensor and AWind libraries
-  Copyright (C)2015 Andrei Degtiarev. All right reserved
+  Copyright (C)2014-2018 Andrei Degtiarev. All right reserved
   
   You can always find the latest version of the library at 
   https://github.com/AndreiDegtiarev/AFrame
--- Log.cpp
+++ Log.cpp
@@ -1,13 +1,13 @@
 /*
-  AFrame - Arduino framework library for ASensor and AWind libraries
-  Copyright (C)2015 Andrei Degtiarev. All right reserved
-  
-  You can always find the latest version of the library at 
-  https://github.com/AndreiDegtiarev/AFrame
+AFrame - Arduino framework library for ASensor and AWind libraries
+Copyright (C)2014-2018 Andrei Degtiarev. All right reserved
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the MIT license.
-  Please see the included documents for further information.
+You can always find the latest version of the library at
+https://github.com/AndreiDegtiarev/AFrame
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the MIT license.
+Please see the included documents for further information.
 */
 #include "Log.h"
 
